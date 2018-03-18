0cc1878459c372fc468cd362a00ea93d7453b46f
harutyunyananahit <harutyunyananahit14@gmail.com>
2018-03-19 01:41:11 +0400

    solve new tasks

--- homework2.cpp
+++ homework2.cpp
@@ -1,24 +1,160 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 template <typename Iter>
-int CountMin(Iter beg, Iter end)
+double middle_arithmetic(Iter beg, Iter end)
 {
-  int min;
-  while(beg != end)
-  {
-    if(*beg < *(beg+1))
-    {
-      min = *beg;
-    }
-    ++beg;
-  }
-  return min;
+	double result = 0;
+	int size = 0;
+	while (beg != end)
+	{
+		result += *beg;
+		++beg;
+		++size;
+	}
+	return result / size;
 }
-int main()
+
+template <typename Iter>
+int min_element(Iter beg, Iter end)
 {
-  int  A[8] = {5, 8, 3, 9, 4, 2, 6, 7};
-  cout << CountMin(A, A+8);
-  return 0;
+	int min = 1111111111;
+	while (beg != end)
+	{
+		if (*beg < min)
+		{
+			min = *beg;
+		}
+		++beg;
+	}
+	return min;
+}
+
+//Task 1
+template<typename Iter>
+unsigned int count_even_numbers(Iter beg, Iter end)
+{
+	unsigned int result = 0;
+	while (beg != end)
+	{
+		if ((*beg % 2) == 0)
+		{
+			++result;
+		}
+		++beg;
+	}
+	return result;
+}
+
+//Task 2
+template<typename Iter, typename T>
+unsigned int count_elements_in_interval(Iter beg, Iter end, T a, T b)
+{
+	unsigned int res = 0;
+	while (beg != end)
+	{
+		if (a <= *beg && *beg <= b)
+		{
+			++res;
+		}
+		++beg;
+	}
+	return res;
+}
+
+//Task3
+template <typename Iter, typename T>
+T closest_number(Iter beg, Iter end, T R)
+{
+	double differ = 11111111111;
+	T result;
+	while (beg != end)
+	{
+		if (abs((*beg) - R) < differ)
+		{
+			differ = abs(*beg - R);
+			result = *beg;
+		}
+		++beg;
+	}
+	return result;
+}
+
+//Task 4
+template <typename Iter>
+int max_odd_element(Iter beg, Iter end)
+{
+	int max = -1111111111;
+	while (beg != end)
+	{
+		if ((*beg % 2) == 1 && *beg > max)
+		{
+			max = *beg;
+		}
+		++beg;
+	}
+	return max;
+}
+
+//Task 6
+template <typename Iter>
+int min_even_element(Iter beg, Iter end)
+{
+	int min = 1111111111;
+	while (beg != end)
+	{
+		if ((*beg % 2) == 0 && *beg < min)
+		{
+			min = *beg;
+		}
+		++beg;
+	}
+	return min;
+}
+
+//Task 7
+template <typename Iter>
+double middle_arithmetic_abs(Iter beg, Iter end)
+{
+	double result = 0;
+	int size = 0;
+	while (beg != end)
+	{
+		result += abs(*beg);
+		++beg;
+		++size;
+	}
+	return result / size;
+}
+
+//Task 8
+template<typename Iter>
+void less_than_middle_arithmetic(Iter beg, Iter end)
+{
+	double midAr = middle_arithmetic(beg, end);
+	while (beg != end)
+	{
+		if (double(*beg) < midAr)
+		{
+			cout << *beg << ' ';
+		}
+		++beg;
+	}
+}
+
+//Task 9
+template <typename Iter>
+unsigned int count_number_of_min_elements(Iter beg, Iter end)
+{
+	int min = min_element(beg, end);
+	unsigned int result = 0;
+	while (beg != end)
+	{
+		if (*beg == min)
+		++result;
+		++beg;
+	}
+	return result;
 }
