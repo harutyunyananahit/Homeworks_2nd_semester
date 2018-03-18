34ef44ceb073f024c29c052aad44a77d71ce989d
harutyunyananahit <32642867+harutyunyananahit@users.noreply.github.com>
2018-03-18 21:28:49 +0400

    Update homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,66 +1,100 @@
 #include <iostream>
-#include <vector>
-using namespace std;
 //Task 1
-/*template<typename ForwardIter>
+template<typename ForwardIter>
 void advance(ForwardIter& it, unsigned int steps)
 {
-  it += steps;
-}*/
-//Task 2
-template<typename InputIt, typename OutputIt>
-void copy(InputIt b, InputIt e, OutputIt o)
-{
-  while(b != e)
-  {
-    *o = *b;
-    o++;
-    b++;
-  }
+it += steps;
 }
+
+//Task 2
+template<typename InputIterator, typename OutputIterator>
+void copy(InputIterator b, InputIterator e, OutputIterator o)
+{
+	while (b != e)
+	{
+		*o = *b;
+		++o;
+		++b;
+	}
+}
+
 //Task 3
-/*template<typename Iter, typename T>
+template<typename Iter, typename T>
 Iter find(Iter b, Iter e, const T& val)
 {
+	while (b != e)
+	{
+		if (*b == val)
+		{
+			return b;
+		}
+		++b;
+	}
+	return e;
 }
+
 //Task 4
 template<typename Iter, typename T>
 Iter remove(Iter b, Iter e, const T& val)
 {
+	Iter result = b;
+	while (b != e)
+	{
+		if (*b != val)
+		{
+			*result = *b;
+			++result;
+		}
+		++b;
+	}
+	return result;
 }
+
 //Task 5
 template<typename Iter, typename T>
 unsigned int count(Iter b, Iter e, const T& val)
 {
+	unsigned int result = 0;
+	while (b != e)
+	{
+		if (*b == val)
+		{
+			++result;
+		}
+		++b;
+	}
+	return result;
 }
+
 //Task 6
 template<typename Iter>
 bool equal_range(Iter b, Iter e, Iter s)
 {
+	while (b != e)
+	{
+		if(!(*b == *s))
+		{
+			return false;
+		}
+		++s;
+		++b;
+		return true;
+	}
 }
+
 //Task 7
 template<typename Iter>
-void sort(Iter b, Iter e)
+void sr(Iter b, Iter e)
 {
-}*/
-int main()
-{
-  vector <int> A;
-  A.push_back(4);
-  A.push_back(8);
-  A.push_back(7);
-  A.push_back(3);
-  A.push_back(1);
-  A.push_back(6);
-  vector <int> M;
-  std::vector<int>::iterator v = A.begin();
-  std::vector<int>::iterator m = A.begin()+6;
-  std::vector<int>::iterator l = M.begin();
-  copy(A.begin(), A.begin()+6, M.begin());
-  for(std::vector<int>::iterator it = M.begin() ; it < M.end(); ++it)
-  {
-    cout << *it;
-  }
-
-  return 0;
+	while (b != e)
+	{
+		Iter k;
+		if (*b > *(b + 1))
+		{
+			*k = *(b + 1);
+			*(b + 1) = *b;
+			*b = *k;
+		}
+		++b;
+	}
 }
